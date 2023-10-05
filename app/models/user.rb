41ce0e5e4a66727ede90172c3a8886b123f706b5
aaaaaakjj <kazya1192@outlook.jp>
2023-10-05 20:52:27 +0900

    ## 新規登録/ユーザー情報,## 新規登録/本人情報確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname , presence: true
+          # 他のバリデーションルールも含めるか、必要に応じて追加してください
+  validates :email, presence: true, uniqueness: true
+          # 他のバリデーションルールも含めるか、必要に応じて追加してください
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true      
+  validates :birth_date, presence: true
+  #has_many[ハズメニー、多数持っている]
+  has_many :items
+  has_many :orders
 end
+
+
+
+
+
+
+
+
+
