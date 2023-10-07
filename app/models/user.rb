b95bb0d576f752b7d93c2255fca585e4891bf703
aaaaaakjj <kazya1192@outlook.jp>
2023-10-07 11:30:14 +0900

    # パスワードの半角英数混合を検証するバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname , presence: true
           # 他のバリデーションルールも含めるか、必要に応じて追加してください
-
-          
+          # パスワードの半角英数混合を検証するバリデーションを追加
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "is invalid. Include both letters and numbers" }        
   validates :last_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid' }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid' }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid' }
