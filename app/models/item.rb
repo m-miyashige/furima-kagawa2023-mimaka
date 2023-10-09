6e005ea3215b9c7923d5d4d28f88337ba4302cc8
m-miyashige <mchr.miyashige@gmail.com>
2023-10-09 14:12:41 +0900

    エラーメッセージの表示、不足していたアソシエーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :sales_status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 
+  
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, numericality: { other_than: 1}
+  validates :sales_status_id, numericality: { other_than: 1}
+  validates :shipping_fee_status_id, numericality: { other_than: 1}
+  validates :prefecture_id, numericality: { other_than: 1}
+  validates :scheduled_delivery_id, numericality: { other_than: 1}
+  validates :price, presence: true
 end
