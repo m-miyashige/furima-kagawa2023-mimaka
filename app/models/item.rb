f68ca63cc245a75323797151b33d65137e20ed81
m-miyashige <mchr.miyashige@gmail.com>
2023-10-09 16:08:33 +0900

    validation messageの変更、priceのvalidation変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,13 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :scheduled_delivery
 
-  
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1}
-  validates :sales_status_id, numericality: { other_than: 1}
-  validates :shipping_fee_status_id, numericality: { other_than: 1}
-  validates :prefecture_id, numericality: { other_than: 1}
-  validates :scheduled_delivery_id, numericality: { other_than: 1}
-  validates :price, presence: true
+  validates :price, presence: true, numericality:{ with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
+  validates :price, numericality:{ in:300..9999999, message: 'is out of setting range' }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank"}
 end
