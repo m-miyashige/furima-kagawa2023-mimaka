81014b7eded710278684554dbf34127be5b1eede
m-miyashige <mchr.miyashige@gmail.com>
2023-10-09 11:35:09 +0900

    DBへのデータの保存機能を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new  
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)  
+  end
+
 end
