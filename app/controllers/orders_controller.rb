b594f8a756d9dbcb38d19e7fa4af3fe08208e8b8
norineko-hi <fragrant.olive.3@gmail.com>
2023-10-15 17:16:44 +0900

    orderページでの商品表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,15 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :shipping_fee_status.name)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "items#index"
-
-  resources :orders, only:[:create, :index]
-
   devise_for :users
-  resources :items, only: [:new, :create]
+  resources :items, only: [:new, :create] do
+    resources :orders, only:[:create, :index]
+  end
 end
