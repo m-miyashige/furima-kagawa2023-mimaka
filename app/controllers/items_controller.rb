df6f63ee255efe7f30b0b28ac54ef34f8a5b5794
aaaaaakjj <kazya1192@outlook.jp>
2023-10-18 11:39:02 +0900

    商品情報編集ページに遷移できること。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  def edit
+    @item = Item.find(params[:id])
+  render :edit
+  end
 
   private
 
