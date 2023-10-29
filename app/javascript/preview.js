7806b16473969dddce83955bebb0b7c7af5a5d37
m-miyashige <mchr.miyashige@gmail.com>
2023-10-29 22:07:46 +0900

    input要素で変化があったことの検出

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,17 @@
 document.addEventListener('turbo:load', function () {
+  // 商品出品ページのフォームを取得
   const newItemForm = document.getElementsByClassName('items-sell-contents');
 
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('preview');
+
+  // 商品出品ページのフォームがないならここで終了
   if (!newItemForm) return null;
-  console.log("previewが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  fileField.addEventListener('change', function (e) {
+  });
+
+
 });
