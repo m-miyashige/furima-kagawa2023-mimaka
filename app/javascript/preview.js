0779afe42ddd75714274ec7450da4cfe1c37190a
m-miyashige <mchr.miyashige@gmail.com>
2023-10-29 22:17:00 +0900

    previewを表示させる

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -11,6 +11,13 @@ document.addEventListener('turbo:load', function () {
   // input要素を取得
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   fileField.addEventListener('change', function (e) {
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+    previewList.appendChild(previewImage);
   });
 
 
